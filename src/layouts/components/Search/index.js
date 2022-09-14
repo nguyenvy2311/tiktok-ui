8f53ec5b5a8a7384684e72bdaf88a221d761f681
nguyenvy2311 <vhnv23111999@gmail.com>
2022-09-14 23:28:17 +0700

    Restructure

--- src/layouts/components/Search/index.js
+++ src/layouts/components/Search/index.js
@@ -4,7 +4,7 @@ import HeadlessTippy from '@tippyjs/react/headless';
 import classNames from 'classnames/bind';
 import { useEffect, useState, useRef } from 'react';
 
-import * as searchServices from '~/apiServices/searchService';
+import * as searchServices from '~/services/searchService';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import AccountItem from '~/components/AccountItem';
 import { SearchIcon } from '~/components/Icons';
